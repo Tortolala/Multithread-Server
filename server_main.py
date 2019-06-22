02ee57c85d8b0637d1f9acc71998ce4e496e5a54
Luis Angel <tortoladev@gmail.com>
2019-06-22 17:46:56 -0600

    Socket connection established | Working with one thread

--- server_main.py
+++ server_main.py
@@ -4,6 +4,8 @@ import time
 import concurrent.futures
 import argparse
 import numpy
+import ast
+import socket
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--threads", required=True)
@@ -25,14 +27,18 @@ def thread_function(name):
             time.sleep(10)
         else:
 
-            task = tasks[0] # chequear
+            task = tasks[0] # chequear para compartir recurso
+            print("Task en thread: ", task)
+            tasks = []
             col = task[0]
             row = task[1]
             con = task[2]
             result = numpy.matmul(col, row)
-        
             # Send result through connection
+            print(result)
+            con.send(bytes(str(result), "utf-8"))
             # Close connection
+            con.close()
 
     logging.info("Thread %s: finishing", name)
 
@@ -42,9 +48,31 @@ if __name__ == "__main__":
                         datefmt="%H:%M:%S")
 
     # CREATE THREAD POOL 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_threads) as executor:
-        executor.map(thread_function, range(number_of_threads))
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_threads) as executor:
+    #     executor.map(thread_function, range(number_of_threads))
+
+    x = threading.Thread(target=thread_function, args=(1,))
+    x.start()
 
     # CREATE SOCKETS
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("192.168.2.15", 1234))
+    s.listen(5)
+    print("Socket created")
+
+    while True:
+        # now our endpoint knows about the OTHER endpoint.
+        (clientsocket, (address,port)) = s.accept()
+        print(f"Connection from {address} with {port}")
+        data=clientsocket.recv(1024).decode("utf-8")
+        print("El cliente dice: ",data)
+        # clientsocket.send(bytes("Hey there!!!","utf-8"))
+        # clientsocket.close()
+        data = ast.literal_eval(data)
+        print("Data 1: ", data)
+        data.append(clientsocket)
+        print("Data 2: ", data)
+        tasks.append(data)
+        print("Data 3: ", tasks)
 
     
