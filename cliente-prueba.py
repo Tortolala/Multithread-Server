a3195ec41a1a6d6ac1abd5e3461c90d286dbd3f0
ricardojmv85 <ricardojmv85@gmail.com>
2019-06-22 17:13:15 -0700

    Update cliente-prueba.py

--- cliente-prueba.py
+++ cliente-prueba.py
@@ -2,13 +2,13 @@
 import socket 
 
 host = '192.168.2.15'
-port = 2004
+port = 80
 BUFFER_SIZE = 2000 
 MESSAGE ='[[1,2,3],[4,5,6]]'
  
 tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 tcpClientA.connect((host, port))
 
-tcpClientA.send(MESSAGE)     
+tcpClientA.send(b'[[1,2,3],[4,5,6]]')     
 response = tcpClientA.recv(BUFFER_SIZE)
-print (" Client received data:", response)
+print(response.decode("utf-8"))
