fe2202a9f1347a19cde11321318f705a11033cea
ricardojmv85 <ricardojmv85@gmail.com>
2019-06-24 22:04:00 -0700

    multithread and locks implementation

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import threading
 import argparse
 import socket
 import time
+from threading import Lock
+lock=Lock()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--threads", required=True)
@@ -17,57 +19,60 @@ m1=str(args["matrix1"])
 m2=str(args["matrix2"])
 out=str(args["output"])
 
-# dataframes initialization
-df1=pd.read_csv(m1+'.csv',header=None)
-df2=pd.read_csv(m2+'.csv',header=None)
-result=pd.read_csv(out+'.csv',header=None)
-tasks = []
-
-# create tasks
-for i in range(len(df1)):
-    for j in range(len(df2)):
-        tasks.append([list(df1.iloc[i]),list(df2[j]),[i,j]])
 
 # runner function for threads
 def thread_function(name):
     global tasks, result
-    while True:
-        if not tasks:
-            print("pos me duermo")
-            time.sleep(10)
-        else:
-            task=tasks[0]
-            tasks.pop(0)
-            req=str(task[0:2])
+    while tasks:
+        lock.acquire()
+        task=tasks[0]
+        tasks.pop(0)
+        lock.release()
+        req=str(task[0:2])
         #envia el request sin el numero de orden
-            print("enviando: ",req)
-            host = '192.168.2.15'
-            port = 80
-            BUFFER_SIZE = 2000 
-            MESSAGE =req
-            res=32
-            result[task[2][0]][task[2][1]]=res
-        '''
-        # connecting with server 
-            tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-            tcpClientA.connect((host, port))
-        # sending request to server
-            tcpClientA.send(b'[[1,2,3],[4,5,6]]')     
-        # waiting for response
-            response = tcpClientA.recv(BUFFER_SIZE)
-            print(response.decode("utf-8"))
-        # inserting response in result df
-            response=int(response)
-            result[task[2][0]][task[2][1]]=res
-        '''
+        print("T",name," enviando: ",req)
+        host = '172.20.10.4'
+        port = 1234
+        BUFFER_SIZE = 2000 
+            # result[task[2][0]][task[2][1]]=response
+           
+        # # connecting with server 
+        #     tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        #     tcpClientA.connect((host, port))
+        # # sending request to server
+        #     tcpClientA.send(req.encode())     
+        # # waiting for response
+        #     response = tcpClientA.recv(BUFFER_SIZE)
+        #     print(response.decode("utf-8"))
+        # # inserting response in result df
+        #     response=int(response)
+        #     result[task[2][0]][task[2][1]]=response
+
+        
             
 
 if __name__ == "__main__":
+    # dataframes initialization
+    df1=pd.read_csv(m1+'.csv',header=None)
+    df2=pd.read_csv(m2+'.csv',header=None)
+    result=pd.read_csv(out+'.csv',header=None)
+    tasks = []
 
+    # create tasks
+    for i in range(len(df1)):
+        for j in range(len(df2)):
+            tasks.append([list(df1.iloc[i]),list(df2[j]),[i,j]])
     # threads initialization
-    x = threading.Thread(target=thread_function, args=(1,))
-    x.start()
-    x.join()
+    threads=[]
+    for i in range(number_of_threads):
+        x = threading.Thread(target=thread_function, args=(i,))
+        threads.append(x)
+        x.start()
+    for i in threads:
+        
+        i.join()
+    
+    
     # taks production
 
 
